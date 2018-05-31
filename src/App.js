1feae49d1cbd6e8a0300fe3187d56dcfc2f9b6fb
Karthik M S <ms18karthik@gmail.com>
2018-05-31 15:12:41 +0530

    Starting Implemention of the Nano Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
+import Nano from './containers/nano/Nano';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Test</p>
+          <Nano />
         </Layout>
       </div>
     );
---
+++ src/containers/nano/Nano.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+
+class Nano extends Component {
+	render(){
+		return(
+			<Aux>
+				<div>Nano</div>
+				<div>Build Controls</div>
+			</Aux>
+		);
+	}
+}
+
+export default Nano;
