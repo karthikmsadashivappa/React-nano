b721f65ec4ee49625b9c945bdef1cdba8793868d
Karthik M S <ms18karthik@gmail.com>
2018-06-06 12:28:43 +0530

    Removing Ingrediants safely

--- src/components/Tiny/BuildControls/BuildControls.js
+++ src/components/Tiny/BuildControls/BuildControls.js
@@ -15,9 +15,11 @@ const BuildControls = (props) => (
 			<BuildControl 
 			key={ctrl.label} 
 			label={ctrl.label}
-			added={() => props.ingredientAdded(ctrl.type)}/>
+			added={() => props.ingredientAdded(ctrl.type)}
+			removed={() => props.ingredientRemoved(ctrl.type)}
+			disabled={props.disabled[ctrl.type]}/>
 		))}
 	</div>
 ); 
 
-export default BuildControls;
+export default BuildControls; 
